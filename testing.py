1c03d2ffc3a120bb97c2d38e3747735746379689
oladeindepaul <anuoluwapooladeinde@gmail.com>
2022-10-06 21:02:10 +0100

    My first time

---
+++ testing.py
@@ -0,0 +1,16 @@
+x = input('Enter first number :')
+y = input('Enter second number :')
+z = input()
+ 
+if (z=='+'):
+     print(x+y)
+elif (z=='*'):
+    print(x*y)
+elif (z=='-'):
+    print(x-y)
+elif (z=='/'):
+    print(x/y)
+
+else:
+    print("not valid")
+
